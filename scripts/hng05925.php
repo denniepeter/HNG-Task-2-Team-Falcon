6a55e0a2298eeffb3cff3f083f5653a70d0e76dc
denniepeter <43421158+denniepeter@users.noreply.github.com>
2020-06-04 15:18:03 +0100

    Another update hng05925
    corrected output format

--- scripts/hng05925.php
+++ scripts/hng05925.php
@@ -5,7 +5,7 @@
     $language =  'PHP';
     $emailaddress = 'dennyokus@gmail.com';
 
-    $tasktwo = "Hello World, this is {$fullname} with HNGi7 ID {$hngid} using {$language} for stage 2 task. {$emailaddress}";
+    $tasktwo = "Hello World, this is {$fullname} with HNGi7 ID {$hngid} using {$language} for stage 2 task. dennyokus@gmail.com"; 
     
 
 
